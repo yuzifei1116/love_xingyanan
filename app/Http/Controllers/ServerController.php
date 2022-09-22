6ef9c6228f4419daba512256e6fb06f5ed554842
zifei yu <1213649307@qq.com>
2022-09-22 14:38:34 +0800

    1

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -13,14 +13,17 @@ class ServerController extends Controller
         # code...
         $config = [
             'app_id' => 'wx440170b2078e530b',
-            // 'app_id' => 'wx461c8601b484bd03',
-            // 'secret' => '58567cc5411fe20a7d105059d283b571',
             'secret' => '75c523a900af69e3ad910647ccaf7c24',
             'token' => 'test'
         ];
         
         $app = Factory::officialAccount($config);
-        
+
+        $user = $app->oauth->scopes(['snsapi_userinfo'])
+                          ->redirect();
+
+        dd($user);
+
         return $app->server->serve();
     }
 }
