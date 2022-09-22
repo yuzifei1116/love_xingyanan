49e1553eb4622e15b6f8868abac982179d760e3d
zifei yu <1213649307@qq.com>
2022-09-22 14:17:10 +0800

    1

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -12,10 +12,10 @@ class ServerController extends Controller
     {
         # code...
         $config = [
-            // 'app_id' => 'wx440170b2078e530b',
-            'app_id' => 'wx461c8601b484bd03',
-            'secret' => '58567cc5411fe20a7d105059d283b571',
-            // 'secret' => '75c523a900af69e3ad910647ccaf7c24',
+            'app_id' => 'wx440170b2078e530b',
+            // 'app_id' => 'wx461c8601b484bd03',
+            // 'secret' => '58567cc5411fe20a7d105059d283b571',
+            'secret' => '75c523a900af69e3ad910647ccaf7c24',
             'token' => 'yuzifei'
         ];
         
