df0e171324ccb0092cc3346e5aebe660de98bca5
zifei yu <1213649307@qq.com>
2022-09-22 14:08:06 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 // 
-Route::post('/server', 'ServerController@server');
+Route::any('/', 'ServerController@server');
